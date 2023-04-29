055d3f90dae71b46b9d48030e10c873a6536a07e
buckydroid <buckydroid@gmail.com>
2023-04-29 17:35:33 +0530

    first commit

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,11 @@
+self.__NEXT_FONT_MANIFEST={
+  "pages": {
+    "/_app": [
+      "static/media/2aaf0723e720e8b9-s.p.woff2",
+      "static/media/2a407a2bac141f7a-s.p.woff2"
+    ]
+  },
+  "app": {},
+  "appUsingSizeAdjust": false,
+  "pagesUsingSizeAdjust": true
+}
